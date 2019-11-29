161df22594ba1b9c6d57beafb51d7b8272c29b59
Josh Ames <joshames@Joshs-MacBook-Pro.local>
2019-11-28 17:06:46 -0700

    Workflows v2

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,16 @@
+$(document).ready(function(){
+  getWorkflows();
+})
+
+function getWorkflows() {
+  var url = "https://api.hubapi.com/automation/v3/workflows/?hapikey=demo"
+
+  $.ajax(url,{success: function(data){
+    console.log(data);
+  }})
+}
+
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
